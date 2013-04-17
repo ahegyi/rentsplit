9d1cc9c47811e33b6facca575d4f20377e817b9b
Aron Hegyi <ahegyi@gmail.com>
2013-04-16 19:16:32 -0700

    First commit, with Devise!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rentsplit::Application.config.secret_token = '424e8b3d7d8ed7934ccd05c15e0abdaa9310ced7e7eae963556ab8d2edcbce84403a409b1ddfbb49f8be842bc9902ac9ac2cd40651632dcca5fbef8ee9fabe2a'
