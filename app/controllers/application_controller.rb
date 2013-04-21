53334444a5a1a77de22cba15104c9d48873d8da5
Aron Hegyi <ahegyi@gmail.com>
2013-04-20 21:07:13 -0700

    Implemented basic navigation for switching between households

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :get_current_household
+
+  private
+
+  def get_current_household
+    # Is there a better way!?!?
+    @current_household_id = nil
+
+    if request.path.start_with?("/households/")
+      @current_household_id = request.path.split("/households/")[1].split("/")[0].to_i
+    end
+
+  end
+
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 class PagesController < ApplicationController
   def home
-    render :layout => "bare"    
+    if user_signed_in?
+      redirect_to households_url
+    else
+      render :layout => "bare"  
+    end  
   end
 end
